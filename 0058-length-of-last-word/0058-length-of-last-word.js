a9d7d55b78601172712e4cd537b5a78b6d900475
Khairul Hasan <44225180+khairul111010@users.noreply.github.com>
2023-08-29 18:11:32 +0600

    Time: 64 ms (13.53%), Space: 41.9 MB (50.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let new_s = s.trim()
-    let length = 0
-    for(let i = new_s.length - 1; i >= 0; i--){
-        length++
-        if(new_s[i] === " " || new_s[i] === ""){
-            length--
-            return length
-        }
-    }
-    return length
+    let arr  = s.trim().split(" ")
+    return arr[arr.length - 1].length
 };
