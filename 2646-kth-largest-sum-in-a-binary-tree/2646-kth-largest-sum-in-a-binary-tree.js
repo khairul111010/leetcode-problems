5100d5e1c159790fc04f55ae7300d4dc4131cba3
Khairul Hasan <44225180+khairul111010@users.noreply.github.com>
2024-10-22 12:55:01 +0600

    Time: 450 ms (61.22%), Space: 97.2 MB (22.45%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.js
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.js
@@ -12,12 +12,82 @@
  * @return {number}
  */
 
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
 
+    // Insert a new element into the heap
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    // Remove the smallest element from the heap
+    extractMin() {
+        const min = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.bubbleDown();
+        }
+        return min;
+    }
+
+    // Helper method to bubble up the last element
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] >= this.heap[parentIndex]) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    // Helper method to bubble down the root element
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let swapIndex = null;
+
+            if (leftChildIndex < length) {
+                if (this.heap[leftChildIndex] < this.heap[index]) {
+                    swapIndex = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                if (
+                    (swapIndex === null && this.heap[rightChildIndex] < this.heap[index]) ||
+                    (swapIndex !== null && this.heap[rightChildIndex] < this.heap[leftChildIndex])
+                ) {
+                    swapIndex = rightChildIndex;
+                }
+            }
+            if (swapIndex === null) break;
+            [this.heap[index], this.heap[swapIndex]] = [this.heap[swapIndex], this.heap[index]];
+            index = swapIndex;
+        }
+    }
+
+    // Return the smallest element in the heap
+    peek() {
+        return this.heap[0];
+    }
+
+    // Get the size of the heap
+    size() {
+        return this.heap.length;
+    }
+}
 
 var kthLargestLevelSum = function(root, k) {
     if(!root) return -1;
     let queue = [root];
-    let levelSums = [];
+     let minHeap = new MinHeap();
 
     while(queue.length > 0){
         let levelSize = queue.length;
@@ -31,14 +101,13 @@ var kthLargestLevelSum = function(root, k) {
             if(node.right) queue.push(node.right);
         }
 
-        levelSums.push(currentLevelSum);
+        if (minHeap.size() < k) {
+            minHeap.insert(currentLevelSum);
+        } else if (currentLevelSum > minHeap.peek()) {
+            minHeap.extractMin();
+            minHeap.insert(currentLevelSum);
+        }
     }
 
-    levelSums.sort((a,b) => b - a);
-
-    if(k <= levelSums.length) {
-        return levelSums[k -1];
-    }
-
-    return -1;
+     return minHeap.size() === k ? minHeap.peek() : -1;
 };
